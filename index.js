2c82f3a60b7034e3f28e85f32da8e649236a7e07
azakyh <azaky.h@gmail.com>
2019-09-05 08:31:32 +0700

    black box

--- index.js
+++ index.js
@@ -5,8 +5,24 @@
     var contexts = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"]
 
     for (var i = 0; i < contexts.length; i++) {
-        var context = contexts[i];
-        gl = canvas.getContext(context)
+        try {
+            var context = contexts[i];
+            gl = canvas.getContext(context);
+        } catch (error) {
+            // Sementara kosong
+        }
+        if (gl) {
+            break;
+        }
     }
+    if(!gl) {
+        alert("webgl tidak ditemukan, tolong gunakan chrome/firefox terbaru"); //ini titik koma-nya ditambahin sendiri wkwk
+    }
+
+    //Bersihkan layar jadi hitam
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Bersihkan buffernya canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
     
 })();
