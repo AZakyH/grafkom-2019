ac4fb2e0824dedcd53318c478910cfcde91bde68
azakyh <azaky.h@gmail.com>
2019-09-05 09:02:51 +0700

    memisah fungsi js

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 (function() {
 
     var canvas = document.getElementById("glcanvas");
-    var gl;
-    var contexts = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"]
-
-    for (var i = 0; i < contexts.length; i++) {
-        try {
-            var context = contexts[i];
-            gl = canvas.getContext(context);
-        } catch (error) {
-            // Sementara kosong
-        }
-        if (gl) {
-            break;
-        }
-    }
-    if(!gl) {
-        alert("webgl tidak ditemukan, tolong gunakan chrome/firefox terbaru"); //ini titik koma-nya ditambahin sendiri wkwk
-    }
+    var gl = glUtils.checkWebGL(canvas);
 
     //Bersihkan layar jadi hitam
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.0, 255.0, 0.0, 1.0);
 
     // Bersihkan buffernya canvas
     gl.clear(gl.COLOR_BUFFER_BIT);
