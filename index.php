856cf247d6afa03511857dda247e967ca6fbc121
A. Zaky H <37254462+AZakyH@users.noreply.github.com>
2019-10-03 09:47:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 
 <h1>Indexes</h1>
 <p>
-  <a href="http://grafkomc-170037.azurewebsites.net/001-muncul_titik/">001-muncul_titik</a>
-  <a href="http://grafkomc-170037.azurewebsites.net/002-tugas_huruf/">002-tugas_huruf</a>
-  <a href="http://grafkomc-170037.azurewebsites.net/003-segitiga_animasi/webgl_c/">003-segitiga_animasi</a>
-  <a href="http://grafkomc-170037.azurewebsites.net/004-tugas_huruf_animasi/">004-tugas_huruf_animasi</a>
+  <a href="http://grafkomc-170037.azurewebsites.net/001-muncul_titik/">001-muncul_titik</a><br>
+  <a href="http://grafkomc-170037.azurewebsites.net/002-tugas_huruf/">002-tugas_huruf</a><br>
+  <a href="http://grafkomc-170037.azurewebsites.net/003-segitiga_animasi/webgl_c/">003-segitiga_animasi</a><br>
+  <a href="http://grafkomc-170037.azurewebsites.net/004-tugas_huruf_animasi/">004-tugas_huruf_animasi</a><br>
 </p>
 
 </body>
