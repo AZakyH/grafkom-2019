6d3f0935383db3ba89e1378bb06b4227468ad7c1
A. Zaky H <37254462+AZakyH@users.noreply.github.com>
2019-10-03 09:46:36 +0700

    index

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo "Hello World!"
-?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Indexes</h1>
+<p>
+  <a href="http://grafkomc-170037.azurewebsites.net/001-muncul_titik/">001-muncul_titik</a>
+  <a href="http://grafkomc-170037.azurewebsites.net/002-tugas_huruf/">002-tugas_huruf</a>
+  <a href="http://grafkomc-170037.azurewebsites.net/003-segitiga_animasi/webgl_c/">003-segitiga_animasi</a>
+  <a href="http://grafkomc-170037.azurewebsites.net/004-tugas_huruf_animasi/">004-tugas_huruf_animasi</a>
+</p>
+
+</body>
+</html>
